5f19d375c68e6b9f24d80adcc5a4fd676c2c7b5e
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-08-17 23:40:40 +0530

    [add] minimal code for container

--- container/src/App.js
+++ container/src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { createBrowserHistory } from "history";
+import MicroFrontend from "./MicroFrontend";
+
+import "./App.css";
+
+const defaultHistory = createBrowserHistory();
+
+const {
+  REACT_APP_HEADER_HOST: headerHost,
+  REACT_APP_BLOGS_HOST: blogHost,
+} = process.env;
+
+function Header({ history }) {
+  return <MicroFrontend history={history} host={headerHost} name="Header" />;
+}
+
+function Blogs({ history }) {
+  return <MicroFrontend history={history} host={blogHost} name="Blogs" />;
+}
+
+function BlogDetail({ history }) {
+  return (
+    <div>
+      <MicroFrontend history={history} host={blogHost} name="Blogs" />
+    </div>
+  );
+}
+
+function Home({ history }) {
+
+  return (
+    <div className="container">
+      <Header />
+      <Blogs />
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/blogdetail/:blogid" component={BlogDetail} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   );
 }
 
