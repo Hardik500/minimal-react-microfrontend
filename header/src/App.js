79834c8cd6d2b8d1d7d3864f98c391fbb8008069
Hardik Khandelwal <hardikkhandelwal500@gmail.com>
2021-08-17 23:39:15 +0530

    [add] minimal code for header

--- header/src/App.js
+++ header/src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createBrowserHistory } from "history";
+import "./App.css";
 
-function App() {
+const defaultHistory = createBrowserHistory();
+
+function App({ history = defaultHistory }) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>Hardik500 (Header Application)</p>
     </div>
   );
 }
